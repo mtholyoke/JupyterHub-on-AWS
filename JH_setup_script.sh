955da5995669f3657ee3bc76ffa69f3b85980086
Prof G <bgebreme@mtholyoke.edu>
2022-11-21 16:52:00 -0500

    included changes from Luke

--- JH_setup_script.sh
+++ JH_setup_script.sh
@@ -4,6 +4,13 @@
 # Variables
 ####################################
 
+## make sure it's being run as root
+the_user=$(whoami) 
+if [ "$the_user" != "root" ] 
+then
+	echo "please run this script as root" && exit
+fi
+
 # course name for shared folder
 course_name="COMSC_243_shared"
 
@@ -34,22 +41,23 @@ secs_to_time_out=5400
 # Implement changes using variables
 ####################################
 
-# consider ensuring underlying packages are present
-sudo apt install python3 python3-dev git curl
+# update all apt listings and ensure underlying packages are installed
+apt update && apt install -y python3 python3-dev git curl
 
 # install tljh variant of JupyterHub
-curl -L https://tljh.jupyter.org/bootstrap.py | sudo -E python3
+#curl -L https://tljh.jupyter.org/bootstrap.py | sudo -E python3
+curl -L https://tljh.jupyter.org/bootstrap.py | python3
 
 # setup shared folder
-sudo mkdir -p /srv/data/$course_name
+mkdir -p /srv/data/$course_name
 cd /etc/skel
-sudo ln -s /srv/data/$course_name $course_name
+ln -s /srv/data/$course_name $course_name
 cd /home/ubuntu
 
 # add administrative users
 for ADMIN in $administrators
 do
-        sudo tljh-config add-item users.admin $ADMIN
+        tljh-config add-item users.admin $ADMIN
 done
 
 # START ADD STUDENTS FROM CSV 
@@ -73,7 +81,7 @@ IFS='	'
 # 1. targets the third column, 2. gets rid of last few lines, 3. gets rid of the first few lines
 while read -r Email
 do
-	sudo tljh-config add-item users.allowed $Email
+	tljh-config add-item users.allowed $Email
 done < <(cut -d '	' -f3 $ROSTER | head -n $ROSTER_LEN | tail -n +4)
 
 # not sure what this does
@@ -82,40 +90,40 @@ IFS=$OLDIFS
 # END ADD STUDENTS FROM CSV 
 
 # key file - move and set permissions
-sudo mv $cert_key /etc/ssl/private
-sudo chown root:root /etc/ssl/private/$cert_key
-sudo chmod 600 /etc/ssl/private/$cert_key
+mv $cert_key /etc/ssl/private
+chown root:root /etc/ssl/private/$cert_key
+chmod 600 /etc/ssl/private/$cert_key
 
 # cert - move and set permissions
-sudo mv $cert /etc/ssl/certs
-sudo chown root:root /etc/ssl/certs/$cert
-sudo chmod 600 /etc/ssl/certs/$cert
+mv $cert /etc/ssl/certs
+chown root:root /etc/ssl/certs/$cert
+chmod 600 /etc/ssl/certs/$cert
 
 # interim cert - move and set permissions
-sudo mv $cert_interm /etc/ssl/certs
-sudo chown root:root /etc/ssl/certs/$cert_interm
-sudo chmod 644 /etc/ssl/certs/$cert_interm
+mv $cert_interm /etc/ssl/certs
+chown root:root /etc/ssl/certs/$cert_interm
+chmod 644 /etc/ssl/certs/$cert_interm
 
 # change config for https
-sudo tljh-config set https.enabled true
-sudo tljh-config set https.tls.key /etc/ssl/private/$cert_key
-sudo tljh-config set https.tls.cert /etc/ssl/certs/$cert
+tljh-config set https.enabled true
+tljh-config set https.tls.key /etc/ssl/private/$cert_key
+tljh-config set https.tls.cert /etc/ssl/certs/$cert
 
-sudo tljh-config reload proxy
+tljh-config reload proxy
 
 
 # setup google authenticator
-sudo tljh-config set auth.GoogleOAuthenticator.client_id $auth_client_id
-sudo tljh-config set auth.GoogleOAuthenticator.client_secret $auth_client_secret
-sudo tljh-config set auth.GoogleOAuthenticator.oauth_callback_url https://$hub_url/hub/oauth_callback
+tljh-config set auth.GoogleOAuthenticator.client_id $auth_client_id
+tljh-config set auth.GoogleOAuthenticator.client_secret $auth_client_secret
+tljh-config set auth.GoogleOAuthenticator.oauth_callback_url https://$hub_url/hub/oauth_callback
 
-sudo tljh-config set auth.type oauthenticator.google.GoogleOAuthenticator
+tljh-config set auth.type oauthenticator.google.GoogleOAuthenticator
 
 
-# sudo tljh-config reload
+# tljh-config reload
 
 # change seconds to time out if notebook is idle
-sudo tljh-config set services.cull.timeout $secs_to_time_out
+tljh-config set services.cull.timeout $secs_to_time_out
 
 
 ####################################
@@ -123,8 +131,8 @@ sudo tljh-config set services.cull.timeout $secs_to_time_out
 ####################################
 
 #don't know why this doesn't work
-#sudo tljh-config reload proxy
-sudo tljh-config reload
+#tljh-config reload proxy
+tljh-config reload
 
 echo "Script complete!"
 echo "Contents of /srv/data/"
@@ -132,4 +140,4 @@ ls -al /srv/data/
 echo "Contents of /etc/skel"
 ls -al /etc/skel
 echo "New config file settings:"
-sudo tljh-config show
+tljh-config show
